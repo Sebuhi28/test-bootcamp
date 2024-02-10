c721b46d39c43b35612ec87699f9af5a62cfe2f4
Sebuhi28 <sebuhihuseynli8@gmail.com>
2024-02-10 22:18:13 +0300

    task

---
+++ test-bootcamp2.js
@@ -0,0 +1,6 @@
+// Task 2: Change the text content of the greeting
+document.getElementById('textButton').addEventListener('click', function() {
+    // TODO: Complete the function to change the text of the 'greeting' element to "Hello, JavaScript!"
+    let greetingElement=document.getElementById('greeting');
+    greetingElement.textContent="Hello, JavaSscript!"
+});
